05359475fdb818e2410cdb2f99945d61e4a33644
Misbah <misbah.ilyas689@gmail.com>
2026-07-31 14:37:58 +0500

    chore: import project files via AEGIS

---
+++ tasks-mangemnt-api/models/index.js
@@ -0,0 +1,6 @@
+import User from "./userModel.js";
+import Task from "./taskModel.js";
+import RefreshToken from "./refreshTokenModel.js";
+import Otp from "./otpModel.js";
+
+export { User, Task, RefreshToken, Otp };
